7d871c6a8b73265621cbc955eb6b3c8285494efc
JL <jli328@uclive.ac.nz>
2022-04-12 19:17:39 +1200

    add lib to gitignore

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -1,5 +1,28 @@
 package main.java.model;
 
+import java.util.ArrayList;
+
 public class Team {
+    private ArrayList<Monster> teamMember;
+
+
+    public void addMonsterToTeam(Monster newMonster) {
+        // we will need to set and check the max teamMember
+        this.teamMember.add(newMonster);
+    }
+
+    /**
+     * A function used to check if the team is full or not.
+     *
+     * @return true if the team is full. Otherwise, return false.
+     */
+    public boolean teamIsFull() {
+        return this.teamMember.size() >= 3;
+    }
+
+    public ArrayList<Monster> getTeamMember() {
+        return this.teamMember;
+    }
+
 
 }
